14e1675d98105ac765ac96014cb1561448e89ef9
Dustin DeVries <dustin.devries@gmail.com>
2012-10-16 08:10:00 -0500

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -f
+
+if [ `which rake | wc -l` == 0 ]; then
+	echo "Error: rake not installed"
+	exit
+fi
+git pull && git submodule init && git submodule update && git submodule status
+cd .vim/bundle/command-t
+rake make
+echo ""
+echo "Install done!"
+echo ""
+echo "Please point your .vim dir and .vimrc file to:"
+echo "   `pwd`/.vim"
+echo "   `pwd`/.vimrc"
+echo ""
+
