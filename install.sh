3293082d716d6c4f4de07da7b8e6a3169d47595b
Dustin DeVries <dustin.devries@gmail.com>
2013-03-23 22:33:00 -0500

    one last update

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -f
 
-if [ `which rake | wc -l` == 0 ]; then
-	echo "Error: rake not installed"
-	exit
-fi
 git pull && git submodule init && git submodule update && git submodule status
-#cd .vim/bundle/command-t
-#rake make
 echo ""
 echo "Install done!"
 echo ""
